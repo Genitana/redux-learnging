6c8812adaa6fda5d24fb8e042662b47110214522
robert <>
2021-11-15 19:13:31 +0800

    运行redux

---
+++ src/action/index.js
@@ -0,0 +1,9 @@
+const sendAction = () => {
+  return {
+    // type: 'send_type',
+    type: '111',
+    value:'我是一个action'
+  }
+}
+
+export  {sendAction}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { sendAction } from '../../action';
+import store from '../../store';
 
 
 class Home extends React.Component {
+
+    handleClick = () => {
+        const action = sendAction();
+        store.dispatch(action);
+    }
+
+    /** 当组件一加载完毕的时候来监听 */
+    componentDidMount(){
+        store.subscribe(() => {
+            console.log('subscribe:', store.getState());
+            this.setState({});
+            
+        });
+    }
+
     render() { 
-        return <button>test</button>;
+        return (
+        <>
+            <button onClick={this.handleClick}>test</button>
+            <div>{store.getState().value}</div>
+        </>
+        )
     }
 }
  
